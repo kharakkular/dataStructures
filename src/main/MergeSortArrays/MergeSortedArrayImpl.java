1c8ef549119e65fbb5ead4c8c7246c293f1e9a50
kharakkular <kharakkular@gmail.com>
2024-12-04 04:26:15 -0500

    compiled and ran MergeSortedArrayImpl

--- src/main/MergeSortArrays/MergeSortedArrayImpl.java
+++ src/main/MergeSortArrays/MergeSortedArrayImpl.java
@@ -1,18 +1,20 @@
+package MergeSortArrays;
+
 public class MergeSortedArrayImpl {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    // public static void main(String[] args) {
+    //     System.out.println("Hello World");
 
-        int[] num1 = { 1,2,3,0,0,0 };
-        int[] num2 = { 2,5,6 };
-        merge(num1, 3, num2, 3);
+    //     int[] num1 = { 1,2,3,0,0,0 };
+    //     int[] num2 = { 2,5,6 };
+    //     merge(num1, 3, num2, 3);
 
-        for(int i=0; i< num1.length; i++) {
-            System.out.println(num1[i]);
-        }
-    }
+    //     for(int i=0; i< num1.length; i++) {
+    //         System.out.println(num1[i]);
+    //     }
+    // }
 
-    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
         int p1 = m-1;
         int p2 = n-1;
         int mainIndex = m+n-1;
--- src/main/Runner.java
+++ src/main/Runner.java
@@ -1,21 +1,32 @@
 package main;
 
+import MergeSortArrays.MergeSortedArrayImpl;
+
 public class Runner {
 	public static void main(String[] args) {
 		
-		Heap heap = new Heap(10);
+		// Heap heap = new Heap(10);
 		
-		heap.insert(2);
-		heap.insert(4);
-		heap.insert(5);
-		heap.insert(7);
-		heap.insert(8);
-		heap.insert(3);
-		heap.insert(11);
-		heap.printHeap();
+		// heap.insert(2);
+		// heap.insert(4);
+		// heap.insert(5);
+		// heap.insert(7);
+		// heap.insert(8);
+		// heap.insert(3);
+		// heap.insert(11);
+		// heap.printHeap();
 		
-		System.out.println("---------------------------");
-		heap.delete(7);
-		heap.printHeap();
+		// System.out.println("---------------------------");
+		// heap.delete(7);
+		// heap.printHeap();
+
+		    int[] num1 = { 1,2,3,0,0,0 };
+		    int[] num2 = { 2,5,6 };
+			MergeSortedArrayImpl mergeSortedArrayImpl = new MergeSortedArrayImpl();
+		    mergeSortedArrayImpl.merge(num1, 3, num2, 3);
+	
+		    for(int i=0; i< num1.length; i++) {
+		        System.out.println(num1[i]);
+		    }
 	}
 }
