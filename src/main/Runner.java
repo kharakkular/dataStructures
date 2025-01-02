c658363cd4e3b0578cad5ca336d1fb68c3905e0e
kharakkular <kharakkular@gmail.com>
2025-01-02 17:11:44 -0500

    Added leetcode problems under UtilityClass as methods

--- src/main/Runner.java
+++ src/main/Runner.java
@@ -1,6 +1,9 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 // import main.MergeSortArrays.MergeSortedArrayImpl;
 import main.random.*;
@@ -9,7 +12,6 @@ public class Runner {
 	public static void main(String[] args) {
 
 		// Heap heap = new Heap(10);
-
 		// heap.insert(2);
 		// heap.insert(4);
 		// heap.insert(5);
@@ -18,52 +20,56 @@ public class Runner {
 		// heap.insert(3);
 		// heap.insert(11);
 		// heap.printHeap();
-
 		// System.out.println("---------------------------");
 		// heap.delete(7);
 		// heap.printHeap();
-
 		// int[] num1 = { 1,2,3,0,0,0 };
 		// int[] num2 = { 2,5,6 };
 		// MergeSortedArrayImpl mergeSortedArrayImpl = new MergeSortedArrayImpl();
 		// mergeSortedArrayImpl.merge(num1, 3, num2, 3);
-
 		// int[] num1 = { 3,2,2,3 };
 		// int[] num1 = { 0,1,2,2,3,0,4,2 };
-
 		// RemoveElement removeElement = new RemoveElement();
-
-		int[] num1 = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+		// int[] num1 = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
 		// int[] num1 = { 1, 1, 2 };
 		// int[] num1 = { 1, 1 };
 		// System.out.println(removeElement.removeElement(num1, 2));
-
 		// for(int i=0; i< num1.length; i++) {
 		// System.out.println(num1[i]);
 		// }
-
 		// RemoveDuplicatesFromSortedArray duplicates = new
 		// RemoveDuplicatesFromSortedArray();
 		// duplicates.removeDuplicates(num1);
-
 		// Remove Duplicates from Array
 		// RemoveDuplicatesFromSortedArray removeDuplicatesFromSortedArray = new
 		// RemoveDuplicatesFromSortedArray();
 		// int remainingDuplicates =
 		// removeDuplicatesFromSortedArray.removeDuplicates(num1);
 		// System.out.println(remainingDuplicates);
-
 		// System.out.println(majorityElement(new int[] { 3,2,3}));
 		// UtilityClass utilityClass = new UtilityClass();
 		// System.out.println(utilityClass.majorityElement(new int[] { 2,2,1,1,1,2,2
 		// }));
-
 		// System.out.println(maxProfit(new int[] { 9,5,0,3,0,7,7,7,4,1,5,0,1,7 }));
 		// System.out.println(maxProfit(new int[] { 7,1,5,3,6,4 }));
-
 		// int num = 1 << 3;
 		// System.out.println(num);
-
 		// System.out.println(romanToInt("MMCDXXV"));
+		// System.out.println(removeDuplicatesMedium(new int[] { 1,1,1,1,2,2,3 }));
+		// System.out.println(removeDuplicatesMedium(new int[] { 1,1,1, 3, 3, 3}));
+		// System.out.println(removeDuplicatesMedium(new int[] { 0,0,1,1,1,1,2,3,3 }));
+		// System.out.println(removeDuplicatesMedium(new int[] { 1, 1, 1, 2, 2, 3 }));
+
+		int[] arr = { 2,3,0,1,4 };
+		// System.out.println(canJump(arr));
+		// System.out.println(jump(arr));
+
+		// System.out.println(divide(10, 2));
+
+		int a = 5;
+		System.out.println(a << 4);
 	}
+
+	
+	
 }
--- src/main/random/UtilityClass.java
+++ src/main/random/UtilityClass.java
@@ -1,6 +1,8 @@
 package main.random;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class UtilityClass {
 	
@@ -58,4 +60,160 @@ public class UtilityClass {
 		}
 		return sum;
 	}
+
+	public static int removeDuplicatesMedium(int[] nums) {
+		int index = 0;
+		int p = index + 1;
+		while (p < nums.length) {
+			int count = 1;
+			while (p < nums.length && nums[index] == nums[p]) {
+				p++;
+				count++;
+				if(count == 2) {
+					nums[++index] = nums[p-1];
+				}
+			}
+			if(p < nums.length) {
+				nums[++index] = nums[p++];
+			}
+		}
+		return index + 1;
+			// count = count > allowedCount ? allowedCount: count;
+			// if(count >= allowedCount) {
+			// } else {
+			// 	nums[++index] = nums[p++];
+			// }
+		// }
+		// 	while(nums[index] == nums[p]) {
+		// 		count = count + 1;
+		// 		p++;
+		// 		if(p >= nums.length) {
+		// 			count = count > allowedCount ? allowedCount: count;
+		// 			// index += count--;
+		// 			for (int i = 1; i < count; i++) {
+		// 				index += i;
+		// 				nums[index] = nums[p-1];
+		// 			}
+		// 			// while (count > 0) {
+		// 			// 	nums[index - count--] = nums[p - 1];
+		// 			// }
+		// 			return index;
+		// 		}
+		// 	}
+		// 	if(count > allowedCount) {
+		// 		nums[index + allowedCount] = nums[p++];
+		// 		index += allowedCount;
+		// 	} else {
+		// 		for (int i = 1; i < count; i++) {
+		// 			index += i;
+		// 			nums[index] = nums[p];
+		// 		}
+		// 		// nums[index+count] = nums[p++];
+		// 		p++;
+		// 		// index += count;
+		// 	}
+		// }
+		// return index + 1;
+	}
+
+	public static void rotate(int[] nums, int k) {
+		if (nums.length > 1)
+			return;
+		k = k % nums.length;
+		int length = nums.length;
+		List<Integer> list = new ArrayList<Integer>();
+		if (k > 0) {
+			for (int i = 0; i < nums.length; i++) {
+				list.add(i + k >= length ? i + k - length : i + k, nums[i]);
+				// temp[i + k >= length ? i + k - length : i + k] = nums[i];
+			}
+	
+		}
+
+		// int[] temp = new int[nums.length];
+		// int length = nums.length;
+		// if(k > 0) {
+		// for (int i = 0; i < nums.length; i++) {
+		// temp[i+k >= length ? i + k - length: i + k] = nums[i];
+		// }
+		// System.arraycopy(temp, 0, nums, 0, nums.length);
+		// }
+	}
+
+
+	public static int lengthOfLastWord(String s) {
+
+		String[] words = s.trim().split(" ");
+
+		return words[words.length - 1].length();
+	}
+
+	public static boolean canJump(int[] nums) {
+		int maxJump = 0;
+		int stepsCount = 0;
+		while (stepsCount < nums.length - 1) {
+			int currentJump = (stepsCount) + nums[stepsCount];
+
+			maxJump = maxJump > currentJump ? maxJump : currentJump;
+			if(maxJump <= stepsCount) {
+				return false;
+			}
+			stepsCount++;
+		}
+
+		return true;
+	}
+
+	public static int jump(int[] nums) {
+		int nearJump = 0;
+		int farJump = 0;
+		int jump = 0;
+		while (farJump < nums.length - 1) {
+			int farthest = 0;
+			for (int i = nearJump; i <= farJump ; i++) {
+				farthest = Math.max(farthest, i + nums[i]);
+			}
+			nearJump = farJump + 1;
+			farJump = farthest;
+			jump++;
+		}
+		return jump;
+	}
+
+	public static int divide(int dividend, int divisor) {
+        
+		if(dividend <= Integer.MIN_VALUE && divisor == -1) {
+			return Integer.MAX_VALUE;
+		}
+
+		if(dividend <= Integer.MIN_VALUE && divisor == 1) {
+			return Integer.MIN_VALUE;
+		}
+
+		if(dividend >= Integer.MAX_VALUE && divisor == 1) {
+			return Integer.MAX_VALUE;
+		}
+
+
+		boolean isNegative = dividend < 0 ^ divisor < 0;
+
+		// absolute values in long variables
+		long absDividend = Math.abs((long) dividend);
+		long absDivisor = Math.abs((long) divisor);
+		int count = 0;
+		while (absDividend >= absDivisor) {
+			int power = 1;
+			while (Math.pow(absDivisor, power) < absDividend) {
+				if(Math.pow(absDivisor, power + 1) > absDividend) {
+					break;
+				}
+				power++;
+			}
+			absDividend -= Math.pow(absDivisor, power);
+
+			count = count + (int) Math.pow(absDivisor, power - 1);
+		}
+
+		return isNegative ? Math.negateExact(count) : count;
+    }
 }
